d0a748333b037aeea02a9bc8f1b7e258896b3fc9
Tse Kit Yam <me@kytse.com>
2017-06-17 01:16:02 +0800

    Implement update error

--- src/PID.cpp
+++ src/PID.cpp
@@ -21,6 +21,9 @@ void PID::Init(double Kp, double Ki, double Kd) {
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
